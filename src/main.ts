f506ade1d2a4c34fa83ecca3f00c8bcac27640f0
wuwei <wuwei@taqu.cn>
2023-07-17 15:21:04 +0800

    feat: 提交测试代码

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import { createPinia } from 'pinia'
 const app = createApp(App)
 app.use(WwIcons)
 console.log('测试本地能否提交~')
+console.log('测试本地能否提交1~')
 
 app.use(createPinia())
 const update = async () => {
